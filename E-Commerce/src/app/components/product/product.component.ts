fcc85158c93f72d390461057634dd2d5db6e2576
KareemWael1 <87460487+KareemWael1@users.noreply.github.com>
2022-10-17 02:34:18 +0200

    First DB-Back-Front request

--- E-Commerce/src/app/components/product/product.component.ts
+++ E-Commerce/src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { BackendService } from './../../services/backend.service';
 import { product } from './../../interfaces/product';
+import { productResponse } from './../../interfaces/productResponse';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,14 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  productRes : productResponse = {
+    products: []
+  };
   products : Array<product> = [];
   constructor(private backend: BackendService) { }
 
   ngOnInit(): void {
-    this.backend.get("/products").subscribe(products => {this.products = products as Array<product>});
+
   }
 
   getFeaturedProducts(){
+    this.backend.get("/products").subscribe(products => {this.productRes = products as productResponse});
+    this.products = this.productRes.products;
+    console.log(this.productRes);
+    console.log(this.products);
     return this.products;
   }
 
